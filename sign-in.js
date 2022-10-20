00a23d552fcdc504adbd2cde443d9091ea7beecb
gouder hicham <gouderhicham@gmail.com>
2022-10-20 18:59:09 +0100

    added for loop

--- sign-in.js
+++ sign-in.js
@@ -1,3 +1,6 @@
-console.table("hicham is good ") //replaced console.log with table 
-console.log("analytics of webpag performance")
-console.log("user info")
+console.table("hicham is good "); //replaced console.log with table
+console.log("analytics of webpag performance");
+console.log("user info");
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
