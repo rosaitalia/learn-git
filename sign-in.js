4a3a9bb8736fbb6cd52068b7450684f4dbec5772
gouder hicham <gouderhicham@gmail.com>
2022-10-21 21:44:13 +0100

    done the requested changes

--- sign-in.js
+++ sign-in.js
@@ -1,9 +1,9 @@
 console.table("hicham is good "); //replaced console.log with table
 console.log("analytics of webpag performance");
 console.log("user info");
-let array = [];
-for (let index = 0; index < array.length; index++) {
-  const element = array[index]; //removed errors from the for loop
-  console.log("user : ", element); // removed empty elements
-  // remove useless code (the log)
+let users = [];
+for (let userOrder = 0; index < users.length; userOrder++) {
+  const user = users[userOrder];
+  console.log("user : ", element);
+  console.log("done!");
 }
