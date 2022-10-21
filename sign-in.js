87551e483831bfd72fcbff8f4688c51a9432feb0
gouder hicham <gouderhicham@gmail.com>
2022-10-21 21:02:01 +0100

    add user info each log

--- sign-in.js
+++ sign-in.js
@@ -1,8 +1,8 @@
 console.table("hicham is good "); //replaced console.log with table
 console.log("analytics of webpag performance");
 console.log("user info");
-let array = []
+let array = [];
 for (let index = 0; index < array.length; index++) {
   const element = array[index]; //removed errors from the for loop
-  console.log("elements")
+  console.log("user : ", element); // removed empty elements
 }
